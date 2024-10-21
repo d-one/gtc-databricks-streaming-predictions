fa9400c6705c6fc485a1178ec393e5085d0ad876
KNI-d-one <KNI-d-one>
2024-10-21 13:13:09 +0000

    delete workflow parameters

--- notebooks/DLT/2 - Silver.py
+++ notebooks/DLT/2 - Silver.py
@@ -23,10 +23,7 @@ dbutils.widgets.text("CATALOG_NAME", "konstantinos_ninas")
 
 # set up catalog name either by workflow parameters or by using current user's id
 user_email = spark.sql('select current_user() as user').collect()[0]['user']
-try:
-    catalog_name = dbutils.widgets.get("CATALOG_NAME")
-except:
-    catalog_name = user_email.split('@')[0].replace(".", "_").replace("-", "_")
+catalog_name = user_email.split('@')[0].replace(".", "_").replace("-", "_")
 
 # COMMAND ----------
 
