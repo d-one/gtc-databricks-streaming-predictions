9c0d5d2ed7cbc67edc86dfc65eedd804d7211fb4
goumenap <goumenap>
2024-11-11 10:15:33 +0000

    catalog name

--- notebooks/GTC - Setting things up.py
+++ notebooks/GTC - Setting things up.py
@@ -15,18 +15,9 @@ import pyspark.sql.types as t
 
 # COMMAND ----------
 
-# ********* workflow parameters ********* #
-# set parameters here only if running notebook, for example:
-# dbutils.widgets.text("CATALOG_NAME", "konstantinos_ninas")
-
-# COMMAND ----------
-
 # set up catalog name either by workflow parameters or by using current user's id
 user_email = spark.sql('select current_user() as user').collect()[0]['user']
-try:
-    catalog_name = dbutils.widgets.get("CATALOG_NAME")
-except:
-    catalog_name = user_email.split('@')[0].replace(".", "_").replace("-", "_")
+catalog_name = user_email.split('@')[0].replace(".", "_").replace("-", "_")
 
 # COMMAND ----------
 
