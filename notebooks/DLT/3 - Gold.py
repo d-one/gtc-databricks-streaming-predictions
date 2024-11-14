e215ed24b7b9f0c133f8ae9f17e9a0f63a6cf743
KNI-d-one <KNI-d-one>
2024-11-14 15:20:01 +0000

    delete

--- notebooks/DLT/3 - Gold.py
+++ notebooks/DLT/3 - Gold.py
@@ -17,12 +17,6 @@ import dlt
 
 # COMMAND ----------
 
-# ********* workflow parameters ********* #
-# set parameters here only if running notebook, for example:
-# dbutils.widgets.text("CATALOG_NAME", "konstantinos_ninas")
-
-# COMMAND ----------
-
 # set up catalog name either by workflow parameters or by using current user's id
 user_email = spark.sql('select current_user() as user').collect()[0]['user']
 try:
